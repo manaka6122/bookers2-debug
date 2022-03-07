a332f56712e2f10a5e9e5cbc18b9e70e3e673362
EC2 Default User <ec2-user@ip-172-31-17-86.ec2.internal>
2022-03-07 23:33:52 +0000

    [Add]投稿数グラフ化

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,9 +13,7 @@ import "popper.js"
 import "bootstrap"
 import '@fortawesome/fontawesome-free/js/all';
 import "../stylesheets/application"
-require("chartkick")
-require("chart.js")
-
+import "chartkick/chart.js"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
